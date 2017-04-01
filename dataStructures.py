6c318955357fc9d9463e6bd8b6ab4f85ce380593
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-04-01 13:28:00 -0400

    Update dataStructures.py

--- dataStructures.py
+++ dataStructures.py
@@ -1,1 +1,18 @@
 # https://docs.python.org/3/tutorial/datastructures.html
+
+# A list is a vector which contains different data types. In the following example,
+# we create a list containing three values - 10, 20, 'abc'.
+>>> a=list()
+>>> a
+[]
+>>> a.append(10)
+>>> a
+[10]
+>>> a.append(20)
+>>> a
+[10, 20]
+>>> a.append('abc')
+>>> a
+[10, 20, 'abc']
+>>>
+
