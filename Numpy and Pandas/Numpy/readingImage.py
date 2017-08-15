651054a1985ea3ceca49215382a4103ada31db76
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-08-15 10:41:51 -0400

    Update readingImage.py

--- Numpy and Pandas/Numpy/readingImage.py
+++ Numpy and Pandas/Numpy/readingImage.py
@@ -1,7 +1,7 @@
 import scipy
 import numpy
 from scipy import misc
-import glob
+
 image=misc.imread('C:/Users/vivek4/Desktop/top.png')
 
 # change image dimension
@@ -11,6 +11,4 @@ import matplotlib.pyplot as plt
 plt.imshow(image)
 plt.show()
 
-b=b=image[0:200,:,:]
-plt.imshow(image)
-plt.show()
+
