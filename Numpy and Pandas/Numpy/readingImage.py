7bc51a7f530bf9ffed27e78ec05bb145b69995a2
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-08-15 10:33:26 -0400

    Create readingImage.py

---
+++ Numpy and Pandas/Numpy/readingImage.py
@@ -0,0 +1,12 @@
+import scipy
+import numpy
+from scipy import misc
+import glob
+image=misc.imread('C:/Users/vivek4/Desktop/top.png')
+
+# change image dimension
+image.shape
+
+import matplotlib.pyplot as plt
+plt.imshow(image)
+plt.show()
